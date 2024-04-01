6fc5f3b3694a93f035ee2f7801209b2cce19e1f4
Aleksandr <fitnes20@gmail.com>
2024-04-01 13:37:37 +0200

    first commit

---
+++ Home.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>
+        Open up App.js to start working on your app 1isojdhugjkzk 22123!
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default Home;
